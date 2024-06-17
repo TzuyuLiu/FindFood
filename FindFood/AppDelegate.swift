8d7960b3f68e04944e94398371c9aac6a55533ca
20001220 - Andy Liu <andy_liu@books.com.tw>
2024-06-17 16:03:44 +0800

    修正 facebook 按下登入出現 app id not found，以及補回 info plist 裡屬於 facebook 的 URL type

--- FindFood/AppDelegate.swift
+++ FindFood/AppDelegate.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
+import FBSDKCoreKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Initialize Facebook SDK
+        FBSDKCoreKit.ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
         return true
     }
 
