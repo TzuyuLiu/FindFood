13ab5cb8c68ba2dfa0d81ff18aaafac0b26cce93
20001220 - Andy Liu <andy_liu@books.com.tw>
2024-06-17 16:08:12 +0800

    包裝程式碼

--- FindFood/AppDelegate.swift
+++ FindFood/AppDelegate.swift
@@ -14,15 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         // Initialize Facebook SDK
-        FBSDKCoreKit.ApplicationDelegate.shared.application(
-            application,
-            didFinishLaunchingWithOptions: launchOptions
-        )
+        setupSignInWithFacebook(application, didFinishLaunchingWithOptions: launchOptions)
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -34,7 +30,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+private extension AppDelegate {
+    @discardableResult
+    private func setupSignInWithFacebook(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        return ApplicationDelegate.shared.application(application,
+                                                      didFinishLaunchingWithOptions: launchOptions)
+    }
+}
