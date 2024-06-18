1f15904b2d6e4161b7e8cccc9f6f59e0df49dd0b
20001220 - Andy Liu <andy_liu@books.com.tw>
2024-06-18 10:58:09 +0800

    新增登出畫面，並且將登入畫面從 tabbar controller 移出

---
+++ FindFood/LogoutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LogoutViewController.swift
+//  FindFood
+//
+//  Created by 劉子瑜-20001220 on 2024/6/18.
+//
+
+import UIKit
+
+class LogoutViewController: UIViewController {
+    var logout: (() -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func tappedLogout() {
+        logout?()
+    }
+}
