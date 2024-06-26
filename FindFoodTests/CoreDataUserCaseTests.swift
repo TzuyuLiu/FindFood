e2ba2db49746977c2f926c9868b2c6f5b5aec5c6
20001220 - Andy Liu <andy_liu@books.com.tw>
2024-06-26 15:20:15 +0800

    登入成功時儲存使用者

--- FindFoodTests/CoreDataUserCaseTests.swift
+++ FindFoodTests/CoreDataUserCaseTests.swift
@@ -9,13 +9,23 @@ import XCTest
 @testable import FindFood
 
 class LocalUserLoader {
-    init(store: UserStore) {
+    private let store: UserStore
 
+    init(store: UserStore) {
+        self.store = store
+    }
+
+    func loginSuccess(_ user: User) {
+        store.save(user)
     }
 }
 
 class UserStore {
     var user: User?
+
+    func save(_ user: User) {
+        self.user = user
+    }
 }
 
 final class CoreDataUserCaseTests: XCTestCase {
@@ -24,5 +34,22 @@ final class CoreDataUserCaseTests: XCTestCase {
         _ = LocalUserLoader(store: store)
 
         XCTAssertNil(store.user)
+    }
+
+    func test_save_requestLoginSuccess() {
+        let store = UserStore()
+        let sut = LocalUserLoader(store: store)
+
+        sut.loginSuccess(makeUser())
+
+        XCTAssertNotNil(store.user)
+    }
+
+    // MARK: Helper
+    private func makeUser() -> User {
+        return User(name: "Andy",
+                    image: nil,
+                    idToken: "12345",
+                    loginType: .facebook)
     }
 }
