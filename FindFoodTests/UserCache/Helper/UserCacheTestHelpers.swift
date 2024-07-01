a88f471a5f3e8604b334c87a6d151acaa8589dd3
20001220 - Andy Liu <andy_liu@books.com.tw>
2024-07-01 10:19:21 +0800

    將重複使用的 function 移動至 helper

---
+++ FindFoodTests/UserCache/Helper/UserCacheTestHelpers.swift
@@ -0,0 +1,23 @@
+//
+//  UserCacheTestHelpers.swift
+//  FindFoodTests
+//
+//  Created by 劉子瑜-20001220 on 2024/7/1.
+//
+
+import Foundation
+@testable import FindFood
+
+func makeUser() -> User {
+    return User(name: "Andy",
+                image: nil,
+                idToken: "12345",
+                loginType: .facebook)
+}
+
+func makeLocalUser() -> LocalUser {
+    return LocalUser(name: "Andy",
+                image: nil,
+                idToken: "12345",
+                loginType: "facebook")
+}
